9493491758fe7c97971d7922495b48724fa6a9d3
abdulib <abdulrahmon.ibrahim@yahoo.com>
2019-06-07 15:50:39 -0500

    Building interactive viz

--- js/app.js
+++ js/app.js
@@ -49,9 +49,82 @@ d3.csv("data/data.csv", function(error, data) {
   var smokers = data.map(data => data.smokers)
   var poverty = data.map(data => data.poverty)
 
-  console.log("healthcare:", healthcare)
+  // console.log("healthcare:", healthcare)
 
-\
+  //  Create the scales for the chart
+
+  var healthMin = d3.min(healthcare)
+  console.log(healthMin)
+
+  var healthMax = d3.max(healthcare)
+  console.log(healthMax)
+
+
+  var ageMin = d3.min(age)
+  console.log(ageMin)
+
+  var ageMax = d3.max(age)
+  console.log(ageMax)
+
+  // scale x to chart width
+  var xLinearScale = d3.scaleLinear()
+  .domain([d3.min(age) - 0.5, d3.max(age)])
+  .range([0, width]);
+
+  // scale y to chart height
+  var yLinearScale = d3.scaleLinear()
+  .domain([d3.min(healthcare) - 1, d3.max(healthcare)])
+  .range([height, 0]);
+
+
+  // console.log(xLinearScale.bandwidth());
+
+  // Set up the y-axis domain
+  // create axes
+  var yAxis = d3.axisLeft(yLinearScale);
+  var xAxis = d3.axisBottom(xLinearScale);
+
+
+  // set x to the bottom of the chart
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(xAxis);
+
+  // set y to the y axis
+  var g = chartGroup.append("g");
+  yAxis(g);
+
+
+  // Append Data to chartGroup
+  var scattterChart  = chartGroup.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.age))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", 10)
+    .attr("fill", "green")
+    .attr("opacity", ".5");
+
+
+
+
+
+  console.log(scattterChart)
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
 });
 
 
